19508a8c1d1d65444341268b63ad46d64d5344df
katalav <194138161+katalav@users.noreply.github.com>
2025-08-27 14:32:03 +0300

    alustus

---
+++ app.js
@@ -0,0 +1,18 @@
+// KIRJASTOT 
+//-----------
+
+const express = require('express');
+const {engine} = require('express-handlebars');
+
+// Luodaan palvelin
+const app = express();
+
+// Määritellään TCP-portti, jota palvelin kuuntelee
+const PORT = ;
+
+// Määritellään polut kansioihin
+
+// Määritellään URL-reitit
+
+//  Määritellään palvelin
+
